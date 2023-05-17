f42ba08975ae3774c8867ddf4aea6eafed771c9c
Luis Molina <Luis.Molina1@alu.uclm.es>
2023-05-18 00:41:56 +0200

    Desarrollo de UserControl en batalla
    - Se ha desarrollado el UserControl de Charmander para que se ajuste automáticamente
    - Batalla de 1 jugador -> mejoras en entorno visual + código

--- CombatePokemon1Player.xaml.cs
+++ CombatePokemon1Player.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,12 +27,46 @@ namespace PokeGo
     {
         public static String pokemonSeleccionado;
 
+        private UserControl poke_1;
+        private UserControl poke_2;
+
+        ucVisorCharmander charmander;
+
         public CombatePokemon1Player()
         {
             this.InitializeComponent();
-            
+            poke_1 = null;
+            poke_2 = null;
+            charmander = new ucVisorCharmander();
+
+            setUserUserControl(poke_1, charmander, relative_poke_izquierda);
+            animacPermanentes(charmander.animFuegos);
         }
 
+        /// <summary>
+        /// Establece el control de usuario
+        /// </summary>
+        /// <param name="establecer_poke_activo"></param>
+        /// <param name="poke_seleccionado_usu"></param>
+        /// <param name="panel"></param>
+        private void setUserUserControl(UserControl establecer_poke_activo, UserControl poke_seleccionado_usu, RelativePanel panel)
+        {
+            if(establecer_poke_activo == null){
+                establecer_poke_activo = poke_seleccionado_usu;
+                panel.Children.Add(poke_seleccionado_usu);
+            }
+            else
+            {
+                panel.Children.Clear();
+            }
+        }
+
+
+        /// <summary>
+        /// Establecer el pokemon 
+        /// seleccionado de otra ventana
+        /// </summary>
+        /// <param name="pk"></param>
         public static void setPokemonSeleccionado(String pk)
         {
             pokemonSeleccionado = pk;
@@ -48,5 +83,18 @@ namespace PokeGo
             Combate.frInicio.Navigate(typeof(SeleccionPokemon1Player));
         }
 
+
+        /// <summary>
+        /// Lanza animaciones permanentes
+        /// </summary>
+        /// <param storyBoard="sbA"></param>
+        private void animacPermanentes(Storyboard sbA)
+        {
+            if (sbA != null)
+            {
+                sbA.RepeatBehavior = RepeatBehavior.Forever;
+                sbA.Begin();
+            }
+        }
     }
 }
